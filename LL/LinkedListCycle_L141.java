4a1759972725edf5badf2d66b71d4128f5780460
Akash Choudhary <Akash Choudhary@LENOVOLEGION5>
2025-02-03 14:45:44 +0530

    Commit 2

--- LL/LinkedListCycle_L141.java
+++ LL/LinkedListCycle_L141.java
@@ -1,32 +1,22 @@
 package AkashChoudhary_22BCS13892.LL;
 
-class ListNode {
-    int val;
-      ListNode next;
-      ListNode(int x) {
-          val = x;
-          next = null;
-      }
-}
-
 public class LinkedListCycle_L141 {
-        public boolean hasCycle(ListNode head) {
-            if (head == null || head.next == null) {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
                 return false;
             }
-
-            ListNode slow = head;
-            ListNode fast = head.next;
-
-            while (slow != fast) {
-                if (fast == null || fast.next == null) {
-                    return false;
-                }
-                slow = slow.next;
-                fast = fast.next.next;
-            }
-
-            return true;
+            slow = slow.next;
+            fast = fast.next.next;
         }
+
+        return true;
     }
 }
---
+++ LL/ListNode.java
@@ -0,0 +1,10 @@
+package AkashChoudhary_22BCS13892.LL;
+
+class ListNode {
+    int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+}
